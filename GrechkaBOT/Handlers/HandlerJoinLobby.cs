bfd77f1972756ab833136a65c79a90893025b6ae
SylvesterFox <kit28nik@gmail.com>
2023-11-19 01:31:25 +0300

    base HandlerJoinLobby

--- GrechkaBOT/Handlers/HandlerJoinLobby.cs
+++ GrechkaBOT/Handlers/HandlerJoinLobby.cs
@@ -1,5 +1,6 @@
 
 using Discord.WebSocket;
+using GrechkaBOT.Database;
 
 namespace GrechkaBOT.Handlers
 {
@@ -20,20 +21,47 @@ namespace GrechkaBOT.Handlers
             _client.UserVoiceStateUpdated += OnCreatePrivateRoom;
         }
 
+        private async Task<bool> GetIDLobby(long ChannelId) 
+        {
+            var _dblobbyId = new ModelRoomsLobby { lobby_id = ChannelId };
+            ModelRoomsLobby _get = DatabasePost.GetIdChannelLobby<ModelRoomsLobby>(_dblobbyId);
+            if (_get != null) {
+                return true;
+            }
+
+            return false;
+        }
+
         private async Task OnCreatePrivateRoom(SocketUser user, SocketVoiceState state1, SocketVoiceState state2)
         {
             if (user.IsBot)
                 return;
 
             if (state2.VoiceChannel != null) {
-                Console.WriteLine($"User {user.Username}");
-                // await CreateRoom();
+                bool _checkLobby = await GetIDLobby((long)state2.VoiceChannel.Id);
+                Console.WriteLine(state2.VoiceChannel.ConnectedUsers.Count);
+                if (_checkLobby)
+                    await CreateRoom(user);
+                    
+            }
+
+            if (state1.VoiceChannel != null) {
+                int _countVoiceUser = state1.VoiceChannel.ConnectedUsers.Count;
+                if (_countVoiceUser == 0) {
+                    await DestroyerRoom();
+                    return;
+                }
+                return;
             }
         }
 
-        // private async Task CreateRoom() 
-        // {
+        private async Task CreateRoom(SocketUser UserOwner) 
+        {
+            Console.WriteLine("Create room OK");
+        }
 
-        // }
+        private async Task DestroyerRoom() {
+            Console.WriteLine("null");
+        }
     }
 }

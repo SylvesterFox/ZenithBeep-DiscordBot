b68ee078cac7f410ba47c5f0609bdf544b67ef5d
Sylvester Alderson <51517881+SylvesterFox@users.noreply.github.com>
2024-02-18 15:48:52 +0300

    Header components

---
+++ ZenithBeepDeshboard/Client/zenithbeep-dashboard-react/src/components/header/header.tsx
@@ -0,0 +1,48 @@
+import logo from '../../static/logo.svg'
+import "./Header.css"
+
+function Header () {
+    return (
+            <header className='header'>
+            <div className="container">
+                <div className="header__row">
+                    <div className="elements__first">
+                        <div className='header__logo'>
+                            <a href='#' className='logo__link'>
+                                <img className='logo__image' src={logo} alt="logo" />
+                                <span>ZenithBeep</span>
+                            </a>
+                        </div>
+                        <nav className='header__nav__first'>
+                            <ul>
+                                <li>
+                                    <a href="#!" className='docs'><span></span>DOCS</a>
+                                </li>
+                                <li>
+                                    <a href="#!" className='commands'><span></span>COMMANDS</a>
+                                </li>
+                                <li className='list__header'>
+                                    <a href="#!" className='support_us'><span></span>SUPPORT US</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className='elements__second'>
+                        <nav className="header__nav__second">
+                            <ul>
+                                <li>
+                                    <a href="#!" className="link_nav"><span></span></a>
+                                </li>
+                                <li>
+                                    <a href="#!" className='Sign_in'><span></span>SIGN IN</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
